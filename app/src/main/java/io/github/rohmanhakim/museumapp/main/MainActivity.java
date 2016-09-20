bfc6b1f452905d7998b3f3d7fd8812787800e38e
rohmanhakim <rohmanhakim@live.com>
2016-09-20 16:04:40 +0700

    Added RxJava for Retrofit handling

--- app/src/main/java/io/github/rohmanhakim/museumapp/main/MainActivity.java
+++ app/src/main/java/io/github/rohmanhakim/museumapp/main/MainActivity.java
@@ -2,24 +2,26 @@ package io.github.rohmanhakim.museumapp.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
 import io.github.rohmanhakim.museumapp.MuseumApp;
 import io.github.rohmanhakim.museumapp.R;
-import io.github.rohmanhakim.museumapp.api.model.GetMuseumByLatLongResponse;
-import io.github.rohmanhakim.museumapp.api.service.MuseumApiService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import io.github.rohmanhakim.museumapp.api.service.museum.IMuseumAPI;
+import io.github.rohmanhakim.museumapp.api.service.museum.MuseumAPIService;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements MainView {
 
     @Inject
-    MuseumApiService museumApiService;
+    MuseumAPIService museumAPIService;
 
+    MainViewModel viewModel;
     TextView textHello;
+    Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         ((MuseumApp) getApplication()).getMuseumComponent().inject(this);
 
+        viewModel = new MainViewModel(this,museumAPIService);
+
         textHello = (TextView) findViewById(R.id.text_hello);
+        btnTest = (Button) findViewById(R.id.btn_test);
 
-        museumApiService.getMuseumByLatLong(-8.2838530,115.3644660).enqueue(new Callback<GetMuseumByLatLongResponse>() {
-            @Override
-            public void onResponse(Call<GetMuseumByLatLongResponse> call, Response<GetMuseumByLatLongResponse> response) {
-                textHello.setText(response.body().data.get(0).nama);
-            }
-
-            @Override
-            public void onFailure(Call<GetMuseumByLatLongResponse> call, Throwable t) {
-                textHello.setText(t.getMessage());
-            }
-        });
+        btnTest.setOnClickListener(v -> viewModel.getMuseumByLatLong(-8.2838530, 115.3644660));
     }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void setMuseumName(String name) {
+        textHello.setText(name);
+    }
+
 }
---
+++ app/src/main/java/io/github/rohmanhakim/museumapp/main/MainView.java
@@ -0,0 +1,9 @@
+package io.github.rohmanhakim.museumapp.main;
+
+/**
+ * Created by rohmanhakim <rohmanhakim@live.com> on 9/20/16 14:00.
+ */
+public interface MainView {
+    void showError(String message);
+    void setMuseumName(String name);
+}
